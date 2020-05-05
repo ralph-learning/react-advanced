8eff0f9edd1d32a7c7cf80c5ef4158dade2bc0cb
Ralph Effting <ralfting@gmail.com>
2020-05-05 09:12:14 -0300

    Add NProgress

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 import styled from 'styled-components';
+import NProgress from 'nprogress';
 
 import Nav from './Nav';
+
+Router.onRouteChangeStart = function() {
+  NProgress.start();
+}
+
+Router.onRouteChangeComplete = function() {
+  NProgress.done();
+}
+
+Router.onRouteChangeError = function() {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
